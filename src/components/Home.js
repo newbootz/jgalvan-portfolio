426842644817864bb8cdc99333146893f444e744
Galvan <jgalvan@aus1m-techops88.ad.ea.com>
2021-01-10 13:09:29 -0600

    styling blog post page

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <main>
             <img src={image} alt="Night Sky" 
-            className="absolute object-cover w-full h-full opacity-100" />
+            className="absolute object-cover w-full h-full opacity-100 bg-fixed" />
             <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
                 <h1 className="text-6xl text-indigo-900 font-bold cursive leading-none lg:leading-snug home-name">Hi. I'm Jesus.</h1>
             </section>
--- src/components/Post.js
+++ src/components/Post.js
@@ -22,21 +22,20 @@ export default function Post() {
             .catch(console.error);
     }, []);
     return (
-        <main className="bg-green-100 main-h-sceen p-12">
-            <section className="container mx-auto">
+        <main className="bg-indigo-200 min-h-sceen p-12">
+            <section className="container mx-auto my-auto my-8 min-h-sceen">
                 <h1 className="text-5xl flex justify-center cursive">Blog Post Page</h1>
-                <h2 className="text-lg text-gray-600 flex justify-center mb-12">Welcome to my page of blog posts</h2>
+                <h2 className="text-lg text-indigo-600 flex justify-center mb-12">Welcome to my Blog Posts Page</h2>
                 <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
                     { postData && postData.map((post, index) => (
                     <article>
                         <Link to={ "/post/" + post.slug.current} key={post.slug.current}>
-                        <span className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-green-400"
-                        key={index}>
+                        <span className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-indigo-800" key={index}>
                             <img src={post.mainImage.asset.url}
                             alt={post.mainImage.alt}
                             className="w-full h-full rounded-r object-cover absolute"/>
                             <span className="block relative h-full flex justify-end items-end pr-4 pb-4">
-                                <h3 className="text-gray-800 text-lg font-blog px-3 py-4 bg-red-700 text-red-100 bg-opacity-75 rounded">
+                                <h3 className="text-blue-400 text-lg font-blog px-3 py-4 bg-indigo-700 bg-opacity-90 rounded-full hover:bg-red-400 hover:text-indigo-900 shadow-2xl ring-4 ring-indigo-300">
                                 {post.title}
                                 </h3>
                             </span>
