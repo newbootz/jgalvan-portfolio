43cf3012e6ccc4a3d2607e4e672555a309fc3881
Galvan <jgalvan@aus1m-techops88.ad.ea.com>
2021-01-20 21:01:41 -0600

    dependency fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import react from "react";
+import React from "react";
 import image from "../octocat.jpg"
 
 export default function Home() {
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,4 +1,4 @@
-import react, {useState, useEffect} from "react";
+import React, {useState, useEffect} from "react";
 import sanityClient from "../client.js"
 import { Link } from "react-router-dom";
 
